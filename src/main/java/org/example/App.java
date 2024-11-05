c84f65675f38599e71208e01403f10d647644e18
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-11-05 11:07:33 +0100

    Modified Property datatype and used the JsonToProperties class to convert the json file to a lava list.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.List;
 import org.example.logic.TestFunctions;
 import org.example.properties.District;
+import org.example.properties.JsonToProperties;
 import org.example.properties.Property;
 import org.example.properties.Rooms;
 
@@ -13,10 +15,11 @@ import org.example.properties.Rooms;
  * @version 1.0
  */
 public class App {
-  public static void main(String[] args) {
-    Property property1 = new Property(2020, District.SISSACH, Rooms.FIVE_PLUS, 50000);
-    Property property2 = new Property(2015, District.ARLESHEIM, Rooms.FOUR, 30000);
-    List<Property> properties = List.of(property1, property2);
+  public static void main(String[] args) throws IOException {
+
+    List<Property> properties =
+        JsonToProperties.convertJsonToProperties("src/main/resources/properties.json");
+
     TestFunctions.testFunction(properties);
   }
 }
