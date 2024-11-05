055116af1a067af24c6e9a72f0756785c277e54d
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-11-05 08:59:26 +0100

    Execute all logic methods with temporary data.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import java.util.List;
+import org.example.logic.TestFunctions;
+import org.example.properties.District;
+import org.example.properties.Property;
+import org.example.properties.Rooms;
+
 /**
- * Hello world!
+ * This class is responsible for executing the logic methods.
  *
+ * @author Lorenz Boss
+ * @version 1.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    Property property1 = new Property(2020, District.SISSACH, Rooms.FIVE_PLUS, 50000);
+    Property property2 = new Property(2015, District.ARLESHEIM, Rooms.FOUR, 30000);
+    List<Property> properties = List.of(property1, property2);
+    TestFunctions.testFunction(properties);
+  }
 }
