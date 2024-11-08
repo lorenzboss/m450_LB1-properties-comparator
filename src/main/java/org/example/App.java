5f57fafd013ee2ef0a92b2920cfac7ed5acc0ec0
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-11-08 09:21:56 +0100

    Execute the logic methods of the PropertyService and the DistrictService.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.example.districts.District;
-import org.example.districts.JsonToDistricts;
-import org.example.logic.TestFunctions;
-import org.example.properties.JsonToProperties;
-import org.example.properties.Property;
+import org.example.districts.DistrictService;
+import org.example.properties.PropertyService;
 
 /**
  * This class is responsible for executing the logic methods.
@@ -16,15 +10,12 @@ import org.example.properties.Property;
  * @version 1.0
  */
 public class App {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
 
-    List<Property> properties =
-        JsonToProperties.convertJsonToProperties("src/main/resources/properties.json");
+    PropertyService propertyService = new PropertyService();
+    DistrictService districtService = new DistrictService();
 
-    TestFunctions.testFunction(properties);
-
-    List<District> districts =
-        JsonToDistricts.convertJsonToDistricts("src/main/resources/districts.json");
-    System.out.println(districts);
+    propertyService.executePropertyLogic();
+    districtService.executeDistrictLogic();
   }
 }
